0ba979b35c332513fb276e2f296072e82a1d36e8
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-01-15 21:07:22 +0200

    adds matrix creation tool

--- code/reshape_matrix.py
+++ code/reshape_matrix.py
@@ -8,10 +8,31 @@ class Matrix:
         self.cols = N_COLS
         self.rows = N_ROWS
         self.size = [N_ROWS, N_COLS]
+        self.no_elems = N_COLS * N_ROWS
 
-    def CreateMatrix(self):
+    def LL_CreateMatrix(self):
         m = np.ndarray(shape=(self.rows, self.cols), dtype=int)
         return m
 
-    def ShowMatrix(self):
+    def ShowMatrix(self, matrix):
+        ret_m = matrix
+        return ret_m
+
+    def CreatePairIndexMatrix(self):
+        N = self.rows
+        M = self.cols
+        pairs = [[i, j] for i in range(N + 1) for j in range(M + 1)]
+
+        # create the matrix from the list of pairs
+        count = 0
+        ret_mat = []
+        for id in range(M):
+            local_line = []
+            for id_2 in range(N):
+                local_line.append(pairs[id_2 + count])
+            count = count + N
+            ret_mat.append(local_line)
+        return ret_mat
+
+    def PrintMatrix(self):
         print(self.CreateMatrix())
--- code/test-reshape.py
+++ code/test-reshape.py
@@ -1,9 +1,18 @@
 #!/Users/robertpoenaru/.pyenv/shims/python
 
+import numpy as np
 import reshape_matrix as mat
 
 
-M = mat.Matrix(4, 4)
+N = 4
+M = 3
+
+SIZE = N * M
+
+X = mat.Matrix(M, N)
 
 
-M.ShowMatrix()
+paired_matrix = X.CreatePairIndexMatrix()
+
+for line in paired_matrix:
+    print(line)
